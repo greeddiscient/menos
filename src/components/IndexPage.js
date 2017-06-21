6be3b17926b29595306f39848b9f9c5a2b6cc169
Shaun Djuhari <shaundjuhari@berkeley.edu>
2017-06-21 17:08:18 +0800

    button

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -28,7 +28,9 @@ export default class IndexPage extends React.Component {
           <div className= "about-splash-container">
             <h1 className="menos-title">MENtorship to Optimize Society</h1>
             <p className="menos-intro">The pursuit of excellence can be demoralizing. At times, we feel lost - we don&#39;t know what to do or where to go. We need someone to point us in the right direction.<br/><br/>Menos connects you with highly established individuals that will guide you through your journey. Our mentors will give you solid advice based on their years and years of running successful companies.</p>
-            <button className="btn-default btn-lg mentor-about-button">View All Mentors</button>
+            <Link to ='/mentors'>
+              <button className="btn-default btn-lg mentor-about-button">View All Mentors</button>
+            </Link>
         </div>
         </div>
 
